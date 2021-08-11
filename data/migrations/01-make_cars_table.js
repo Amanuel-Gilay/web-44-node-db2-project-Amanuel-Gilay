f9e5c9c566ade211ef0c1e507be45da8f30a4590
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-10 21:57:58 -0700

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,18 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars',tbl => {
+    tbl.increments()
+    tbl.text('car_vin',556).unique().notNullable()
+    tbl.text('car_mileage').notNullable()
+    tbl.text('car_make').notNullable()
+    tbl.text('car_model').notNullable()
+    tbl.text('car_cleantitle').defaultTo(true)
+    tbl.text('car_salvage').defaultTo(false)
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
+
+
+
