4026be28e50bde7849c07ae91cc5375f6cea18dd
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-11 14:53:00 -0700

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,42 +1,20 @@
 const express = require('express');
-const db = require('../../data/db-config');
+//const db = require('../../data/db-config');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  db('cars')
-    .then(cars => {
-      res.json(cars);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to retrieve cars' });
-    });
+router.get('/', async (req, res, next) => {
+      res.json('getting all cars')
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  db('cars').where({ id }).first()
-    .then(car => {
-      res.json(car);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to retrieve car' });
-    });
+router.get('/:id', (req, res, next) => {
+  res.json(`getting car with id ${req.params.id}`)
+  
 });
 
-router.post('/', (req, res) => {
-  const carData = req.body;
-  db('cars').insert(carData)
-    .then(ids => {
-      db('cars').where({ id: ids[0] })
-        .then(newCarEntry => {
-          res.status(201).json(newCarEntry);
-        });
-    })
-    .catch(err => {
-      res.status(500).json({ message: err.message });
-    });
+router.post('/', (req, res, next) => {
+  res.json('posting new car')
+ 
 });
 
 module.exports = router;
